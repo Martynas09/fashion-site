1e3e8934701a6b6a25b59fe5895926ed733589f8
Martynas09 <martynas200012@gmail.com>
2022-10-26 09:11:54 +0300

    real services

--- resources/views/addService.blade.php
+++ resources/views/addService.blade.php
@@ -13,21 +13,21 @@
         <div class="mb-6">
             <label for="base-input" class=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Pavadinimas</label>
             <input name="title" type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            @error('name')
+            @error('title')
             {{ $message }}
             @enderror
         </div>
         <div class="mb-6">
             <label for="message" class=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Aprašymas</label>
             <textarea name="description" id="message" rows="4" class=" p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
-            @error('name')
+            @error('description')
             {{ $message }}
             @enderror
         </div>
         <div class="mb-6">
             <label for="base-input" class=" mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Kaina</label>
             <input name="price" type="number" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            @error('name')
+            @error('price')
             {{ $message }}
             @enderror
         </div>
@@ -35,7 +35,7 @@
             <label class="mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="user_avatar">Nuotraukų įkelimas</label>
             <input class="w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" type="file" name="photo_url[]">
             <button type="button" class="btn-success focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Pridėti daugiau</button>
-            @error('name')
+            @error('photo_url')
             {{ $message }}
             @enderror
         </div>
@@ -46,7 +46,7 @@
                     <button type="button" class="btn-danger focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Pašalinti</button>
                 </div>
             </div>
-            @error('name')
+            @error('photo_url')
             {{ $message }}
             @enderror
         </div>
